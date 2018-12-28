bf10fd6e5e83a3dc612846bf9b9518253250ee93
Riccardo D'Angelo <riccardo@waga.it>
2018-12-28 11:24:41 +0100

    Updated to the new WabootComponent

--- Navbar_Vertical.php
+++ Navbar_Vertical.php
@@ -4,7 +4,7 @@ Component Name: Navbar Vertical
 Description: Navbar Vertical Component
 Category: Layout
 Tags: Navbar
-Version: 1.0.0
+Version: 1.1.0
 Author: Waboot Team <info@waboot.io>
 Author URI: http://www.waboot.io
 */
@@ -34,9 +34,9 @@ class Navbar_Vertical extends \Waboot\Component{
 
 	public function run(){
 		parent::run();
-		$display_zone = $this->get_display_zone();
-		$display_priority = $this->get_display_priority();
-		WabootLayout()->add_zone_action($display_zone,[$this,"display_tpl"],intval($display_priority));
+		if(\method_exists($this,'add_zone_action')){
+			$this->add_zone_action([$this,'display_tpl']);
+		}
 	}
 
 	public function display_tpl(){
